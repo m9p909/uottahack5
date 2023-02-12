15b6c4d93be81c8cf81cf619e61fc49349fd88dd
Jack Clarke <jackfulcher09@gmail.com>
2023-02-12 10:21:21 -0500

    setup middleware

--- patient2/db.py
+++ patient2/db.py
@@ -3,19 +3,29 @@ class User:
     nonce: str
     key: str
     data: map
-    doctorData: map # a copy of data encrypted with the doctor's credentials
+    doctorData: map  # a copy of data encrypted with the doctor's credentials
 
 
 class UserRepo:
-    data =  { }
-    def getUser(self, publicKey: str) ->  User | None:
-        return self.data[publicKey]
+    data = {}
+
+    def getUser(self, publicKey: str) -> User | None:
+        if publicKey in self.data:
+            return self.data[publicKey]
+        return None
+
+    def setUser(self, user: User) -> User | None:
+        if user.publicKey in self.data:
+            self.data[user.publicKey] = user
+            return user
+        return None
+
+    def createUser(self, user: User) -> User | None:
+        self.data[user.publicKey] = user
+
 
 repo = UserRepo()
 
-def getUserRep():
+
+def getUserRepo():
     return repo
-
-
-
-
